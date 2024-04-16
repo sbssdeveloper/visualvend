527f43c5bdb225481e5040f92b13573051772d90
capanicus-systems <capanicus@gmail.com>
2024-04-16 16:07:06 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'auth'], function () use ($router) {
-    //login,logout
-    $router->post('login', 'AuthController@login');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'auth'], function () use ($router) {
+        //login,logout
+        $router->post('login', 'AuthController@login');
+    });
 });
