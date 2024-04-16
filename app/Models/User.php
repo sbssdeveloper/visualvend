9a195afb0ee2c0b602879657537f9fd2b11216a8
capanicus-systems <capanicus@gmail.com>
2024-04-16 16:47:15 +0530

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 {
     use Authenticatable, Authorizable, HasFactory;
 
+    protected $table = 'admin';
     /**
      * The attributes that are mass assignable.
      *
