37552c0f29f3e6fcf124d5d759ba18613451ab7f
capanicus-systems <capanicus@gmail.com>
2024-04-18 18:54:27 +0530

    changes

--- app/Http/Controllers/MachineController.php
+++ app/Http/Controllers/MachineController.php
@@ -25,13 +25,20 @@ class MachineController extends BaseController
 
     public function list(Request $request)
     {
+        $this->validate($request, ['type' => 'required|in:list,pagination', 'length' => 'required_if:type,pagination']);
         $client_id = $request->auth->client_id;
-        $model = Machine::select(["id", "machine_name"])->where('is_deleted', 0);
+        $model = Machine::select(["id", "machine_name as name"])->where('is_deleted', 0);
         if ($client_id > 0) {
             $list   = explode(",", $request->auth->machines);
             $model = $model->whereIn("id", $list);
         }
-        $model = $model->orderBy('machine_name', "ASC")->get();
-        return parent::sendResponse($model, "Success");
+        $model = $model->orderBy('machine_name', "ASC");
+        if ($request->type === "list") {
+            $model = $model->get();
+            return parent::sendResponse($model, "Success");
+        } else {
+            $model = $model->paginate($request->length);
+            return parent::sendResponseWithPagination($model, "Success");
+        }
     }
 }
