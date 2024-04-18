34a4ced087195215f021987abf372299198d4ed7
capanicus-systems <capanicus@gmail.com>
2024-04-18 16:45:10 +0530

    changes

--- public/index.php
+++ public/index.php
@@ -1,31 +1,6 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, HEAD');
 header('Access-Control-Allow-Headers: *');
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
+$app = require __DIR__ . '/../bootstrap/app.php';
 $app->run();
