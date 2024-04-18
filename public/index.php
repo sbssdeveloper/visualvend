f5366f4ce6064a6542ec02ca40931415d753b4ce
capanicus-systems <capanicus@gmail.com>
2024-04-18 16:02:04 +0530

    changes

--- public/index.php
+++ public/index.php
@@ -11,7 +11,8 @@
 |
 */
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE, HEAD');
+header('Access-Control-Allow-Headers: *');
 
 $app = require __DIR__.'/../bootstrap/app.php';
 
