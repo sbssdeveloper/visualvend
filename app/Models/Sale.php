2de833517f115fab4d59b12e95aa1efd538c9efe
capanicus-systems <capanicus@gmail.com>
2024-04-22 17:29:08 +0530

    changes

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -33,7 +33,7 @@ class Sale extends Model
 
 
         if (!empty($search)) {
-            $model =  $model->where("machine_name", 'like', '%' . $request->search . '%');
+            $model =  $model->where("machine_namee", 'like', '%' . $request->search . '%');
         }
 
         if (!empty($start_date) && !empty($end_date)) {
