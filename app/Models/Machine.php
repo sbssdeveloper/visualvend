7eea93bf45525fdb0460d039935b0032c3ef1d89
capanicus-systems <capanicus@gmail.com>
2024-04-22 13:24:41 +0530

    changes

--- app/Models/Machine.php
+++ app/Models/Machine.php
@@ -9,8 +9,9 @@ class Machine extends Model
     protected $table = 'machine';
     protected $fillable = ['*'];
 
-    public static function personal($machines){
-        $array =  self::select("id")->whereRaw(\DB::raw("FIND_IN_SET(id,\"$machines\")", '!=', null))->where("is_deleted",0)->get();
+    public static function personal($machines)
+    {
+        $array =  self::select("id")->whereRaw(\DB::raw("FIND_IN_SET(id,\"$machines\")", '!=', null))->where("is_deleted", 0)->get()->toArray();
         return collect($array);
     }
 }
