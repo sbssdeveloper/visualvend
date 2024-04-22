fe652b7f675e777aff62f0b64e8d1cb121916038
capanicus-systems <capanicus@gmail.com>
2024-04-22 17:39:18 +0530

    changes

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -18,7 +18,7 @@ class Sale extends Model
         $product_id = $request->product_id;
         $search     = $request->search;
 
-        $model =  self::selectRaw("COUNT(*) as vended_items, SUM(CAST(product_price AS DECIMAL(10,6))) as total_sales")->where('is_deleted', '0');
+        $model =  self::selectRaw("COUNT(*) as vended_items, SUM(CAST(product_price AS DECIMAL(10,2))) as total_sales")->where('is_deleted', '0');
 
         if ($machine_id) {
             $model =  $model->where('machine_id', $machine_id);
