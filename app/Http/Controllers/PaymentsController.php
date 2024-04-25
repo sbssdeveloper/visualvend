28112d1e517a3d4d2517335a340311b40acbf5e6
capanicus-systems <capanicus@gmail.com>
2024-04-25 13:31:52 +0530

    changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\AuthRequest;
 use App\Http\Requests\AuthSignupRequest;
 use App\Http\Requests\ResetPasswordRequest;
 use App\Models\Machine;
+use App\Models\RemoteVend;
 use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Repositories\BaseRepository;
@@ -25,20 +26,25 @@ class PaymentsController extends BaseController
 
     public function list(Request $request)
     {
-        $this->validate($request, ['type' => 'required|in:list,pagination', 'length' => 'required_if:type,pagination', 'page' => 'required_if:type,pagination']);
-        $client_id = $request->auth->client_id;
-        $model = Machine::where('is_deleted', 0);
-        if ($client_id > 0) {
-            $list   = explode(",", $request->auth->machines);
-            $model = $model->whereIn("id", $list);
+        $this->validate($request, ['start_date' => 'required|date_format:Y-m-d H:i:s', 'end_date' => 'required|date_format:Y-m-d H:i:s', 'type' => 'required|in:all,approved,declined,timeout,payment_types,machine_types']);
+
+        $client_id  = $request->auth->client_id;
+        $start_date = $request->start_date;
+        $end_date   = $request->end_date;
+        $machine_id = $request->machine_id;
+        $type       = $request->type;
+        $model      = RemoteVend::where('updated_at>=', $start_date)->where('updated_at<=', $end_date);
+        if ($machine_id > 0) {
+            $model  = $model->where("machine_id", $machine_id);
         }
-        $model = $model->orderBy('machine_name', "ASC");
-        if ($request->type === "list") {
-            $model = $model->select(["id", "machine_name as name"])->get();
-            return parent::sendResponse($model, "Success");
-        } else {
-            $model = $model->paginate($request->length);
-            return parent::sendResponseWithPagination($model, "Success");
+        if (in_array($type, ["all", "approved", "declined", "timeout"])) {
+            $model  = $model->where("machine_id", $type);
         }
+        $model = $model->get();
+        return parent::sendResponse($model, "Success");
+    }
+
+    public function activities(Request $request)
+    {
     }
 }
