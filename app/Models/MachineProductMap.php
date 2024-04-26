5fd826814d6f1cd010c86a6be233ec7d9466a582
capanicus-systems <capanicus@gmail.com>
2024-04-26 17:01:16 +0530

    changes

--- app/Models/MachineProductMap.php
+++ app/Models/MachineProductMap.php
@@ -20,7 +20,7 @@ class MachineProductMap extends Model
         if ($model) {
             if ($model->total_quantity == 0) {
                 $model->required_quantity = "0%";
-            }else{
+            } else {
                 $required_quantity = (($model->total_quantity - $model->remaining_quantity) * 100) / $model->total_quantity;
                 $model->required_quantity = "-" . number_format((float)$required_quantity, 1, '.', '') . "%";
             }
@@ -31,7 +31,7 @@ class MachineProductMap extends Model
     public static function refillRequire($params)
     {
         extract($params);
-        $model = self::selectRaw("machine_id, product_id, (product_max_quantity) as total,(product_quantity) as remaining, ((product_max_quantity)-(product_quantity)) as refill, product_name");
+        $model = self::selectRaw("machine_id,machine_name, product_id, (product_max_quantity) as total,(product_quantity) as remaining, ((product_max_quantity)-(product_quantity)) as refill, product_name")->leftJoin("machine", "machine.id", "=", "machine_product_map.machine_id");
         if ($auth->client_id > 0) {
             $model = $model->whereIn("machine_id", $machine_ids)->where("client_id", $auth->client_id);
         }
