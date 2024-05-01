d59b0c9049942c2a720213f1c5be3ebb680e7245
capanicus-systems <capanicus@gmail.com>
2024-05-01 17:35:54 +0530

    changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -171,7 +171,7 @@ class PaymentsController extends BaseController
         $model      = Transaction::selectRaw("IF(pay_method='pay at machine','pay_at_machine',pay_method) as pay_method,remote_vend_log.aisle_number, transactions.amount, transactions.payment_status, machine_id,machine_name,product_id,product_name,transactions.created_at, transactions.response")->leftJoin('remote_vend_log', 'remote_vend_log.vend_id', '=', 'transactions.vend_uuid');
         // ->whereIn("pay_status", ["pay_to_card", "google_pay", "pay_at_machine", "paypal", "after_pay", "apple_pay", "pay at machine"])
         if(!empty($search)){
-            $model  = $model->where(function ($query) use $search {
+            $model  = $model->where(function ($query) use ($search) {
 
             });
         }
