a4392ddf758bbbe3bb97c5946cdfdebc3b7186e2
capanicus-systems <capanicus@gmail.com>
2024-05-01 16:20:04 +0530

    changes

--- app/Models/RemoteVend.php
+++ app/Models/RemoteVend.php
@@ -12,7 +12,7 @@ class RemoteVend extends Model
 
     public function transaction()
     {
-        return $this->hasOne(Transaction::class, 'vend_id','vend_uuid');
+        return $this->hasOne(Transaction::class, 'vend_uuid','vend_id');
     }
 
     public static function recentVend($params)
@@ -23,7 +23,7 @@ class RemoteVend extends Model
         $machine_id = $request->machine_id;
         $product_id = $request->product_id;
         $search     = $request->search;
-        $model =  self::transaction()->where('is_deleted', '0');
+        $model      =  self::with("transaction")->where('is_deleted', '0');
 
         if ($machine_id) {
             $model =  $model->where('machine_id', $machine_id);
