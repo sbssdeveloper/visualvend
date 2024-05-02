cef6867fbd003ad554798dd883aa3864d7a14312
capanicus-systems <capanicus@gmail.com>
2024-05-02 19:16:54 +0530

    changes

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -18,24 +18,30 @@ class Sale extends Model
         $product_id = $request->product_id;
         $search     = $request->search;
 
-        $model =  self::selectRaw("COUNT(*) as vended_items, SUM(CAST(product_price AS DECIMAL(10,2))) as total_sales")->where('is_deleted', '0');
-
+        $model =  self::selectRaw("SUM(CAST(product_price AS DECIMAL(10,2))) as total_sales")->where('is_deleted', '0');
+        $today = self::selectRaw("COUNT(*) as vended_items, SUM(CAST(product_price AS DECIMAL(10,2))) as total_sales")->where('is_deleted', '0')->whereDate("timestamp", date("Y-m-d"));
         if ($machine_id) {
             $model =  $model->where('machine_id', $machine_id);
+            $today =  $today->where('machine_id', $machine_id);
         } else if ($product_id) {
             $model =  $model->where('product_id', $product_id);
+            $today =  $today->where('product_id', $product_id);
         }
         if ($auth->client_id > 0) {
             $model =  $model->where('client_id', $auth->client_id);
+            $today =  $today->where('client_id', $auth->client_id);
             if (count($machine_ids) > 0) {
                 $model =  $model->whereIn("machine_id", $machine_ids);
+                $today =  $today->whereIn("machine_id", $machine_ids);
             } else {
                 $model =  $model->whereIn("machine_id", ["no_machine"]);
+                $today =  $today->whereIn("machine_id", ["no_machine"]);
             }
         }
 
         if (!empty($search)) {
             $model =  $model->whereRaw("machine_name like '%$request->search%'");
+            $today =  $today->whereRaw("machine_name like '%$request->search%'");
         }
 
         if (!empty($start_date) && !empty($end_date)) {
@@ -43,6 +49,7 @@ class Sale extends Model
             $model  = $model->whereRaw("sale_report.timestamp<='$end_date'");
         }
         $model =  $model->get()->first();
+        $model->today_sales = $today->total_sales ?? "0.00";
         return $model;
     }
 }
