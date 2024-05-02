bd868d7926c0712ccba9b68973c3ffb760564952
capanicus-systems <capanicus@gmail.com>
2024-05-02 18:43:23 +0530

    changes

--- app/Models/MachineProductMap.php
+++ app/Models/MachineProductMap.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Sale;
 
 class MachineProductMap extends Model
 {
@@ -13,10 +14,15 @@ class MachineProductMap extends Model
     {
         extract($params);
         $model = self::selectRaw("SUM(IF(product_quantity=0 AND product_max_quantity>0,1,0)) as out_of_stock,SUM(IF(product_quantity>0 AND product_max_quantity>0,1,0)) as in_stock, SUM(product_max_quantity) as total_quantity,SUM(product_quantity) as remaining_quantity");
+        $slowSell = Sale::select("COUNT(*) as count");
+        SELECT COUNT(*) as count, product_id FROM `sale_report` GROUP BY product_id,client_id HAVING count<=CEIL(count/10);
         if ($auth->client_id > 0) {
             $model = $model->whereIn("machine_id", $machine_ids)->where("client_id", $auth->client_id);
+            $slowSell = $slowSell->whereIn("machine_id", $machine_ids)->where("client_id", $auth->client_id);
         }
+        $slowSell = $slowSell->groupBy("product_id,client_id")->havingRaw(DB::raw('count <= CEIL(count/10)'))->get()->count();
         $model = $model->get()->first();
+        dd($slowSell);
         if ($model) {
             if ($model->total_quantity == 0) {
                 $model->required_quantity = "0%";
@@ -25,6 +31,7 @@ class MachineProductMap extends Model
                 $model->required_quantity = number_format((float)$required_quantity, 1, '.', '') . "%";
             }
         }
+        
         return $model;
     }
 
