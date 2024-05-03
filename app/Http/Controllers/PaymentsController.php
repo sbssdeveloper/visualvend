8d2ea4265b2c03e2e896f2da1a9a6448802191b6
capanicus-systems <capanicus@gmail.com>
2024-05-03 16:01:23 +0530

    changes

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -172,7 +172,7 @@ class PaymentsController extends BaseController
         $pay_type   = $request->pay_type;
         $search     = $request->search;
 
-        $model      = Transaction::selectRaw("IF(pay_method='pay at machine','pay_at_machine',pay_method) as pay_method,remote_vend_log.aisle_number, transactions.amount, transactions.payment_status, machine_id,machine_name,product_id,product_name,transactions.created_at, transactions.response, CASE WHEN response LIKE '%VISA%' THEN 'VISA' WHEN response LIKE '%MASTERCARD%' THEN 'MASTERCARD' ELSE 'AMEX' as card_type")->leftJoin('remote_vend_log', 'remote_vend_log.vend_id', '=', 'transactions.vend_uuid');
+        $model      = Transaction::selectRaw("IF(pay_method='pay at machine','pay_at_machine',pay_method) as pay_method,remote_vend_log.aisle_number, transactions.amount, transactions.payment_status, machine_id,machine_name,product_id,product_name,transactions.created_at, transactions.response, CASE WHEN response LIKE '%VISA%' THEN 'VISA' WHEN response LIKE '%MASTERCARD%' THEN 'MASTERCARD' ELSE 'AMEX' END as card_type")->leftJoin('remote_vend_log', 'remote_vend_log.vend_id', '=', 'transactions.vend_uuid');
         // ->whereIn("pay_status", ["pay_to_card", "google_pay", "pay_at_machine", "paypal", "after_pay", "apple_pay", "pay at machine"])
         
         if (!empty($search)) {
