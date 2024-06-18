1a2e9ff3eec16483d20a857709bdaad37b9b0846
capanicus-systems <capanicus@gmail.com>
2024-06-18 13:38:14 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ $router->group(['prefix' => 'v1'], function () use ($router) {
         $router->post('product/delete',         'ProductController@delete');
         $router->post('product/delete/bulk',    'ProductController@bulkDelete');
         $router->post('product/upload',         'ProductController@upload');
+        $router->post('product/bulk/update',    'ProductController@bulkUpdate');
 
         /****************************CATEGORY******************************/
         $router->get('category/list',          'CategoryController@dropdownList');
