780b70b18d003e3b73d6e3e36be81723df0697d9
capanicus-systems <capanicus@gmail.com>
2024-06-19 12:21:27 +0530

    changes

--- app/Http/Helpers/RequestHelper.php
+++ app/Http/Helpers/RequestHelper.php
@@ -64,7 +64,6 @@ class RequestHelper
         }
 
         if ($request->has("product_category") && !empty($request->product_category)) {
-            dd($request->product_category);
             foreach ($request->product_category as $value) {
                 $categories[] = [
                     "product_id" => $array["product_id"], "category_id" => $value, "client_id" => $client_id
