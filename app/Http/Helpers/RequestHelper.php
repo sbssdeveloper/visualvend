0b9d8252dc9d097189bdbbcbcb6a6cff908db7ff
capanicus-systems <capanicus@gmail.com>
2024-06-19 16:38:25 +0530

    changes

--- app/Http/Helpers/RequestHelper.php
+++ app/Http/Helpers/RequestHelper.php
@@ -81,7 +81,10 @@ class RequestHelper
     {
         $categories = [];
         $data       = $request->only("others", "verification_method", "product_age_verify_minimum", "product_age_verify_required", "product_size_unit", "product_size_amount", "promo_text", "more_info_text", "product_discount_code", "product_status", "vend_quantity", "product_caption", "product_classification_no", "product_sku", "product_grading_no", "product_batch_expiray_date", "product_batch_no", "product_description", "discount_price", "product_price", "product_name");
-
+        $client_id                      = $request->auth->client_id;
+        if ($request->auth->client_id <= 0) {
+            $client_id                  = $request->client_id;
+        }
         $array                          = array_filter($data, function ($var) {
             return !empty($var) && $var != "null";
         });
