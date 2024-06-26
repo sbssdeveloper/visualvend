1f9bca39177cf93a983b079e0411f09adb1d7507
capanicus-systems <capanicus@gmail.com>
2024-06-26 11:22:07 +0530

    changes

--- app/Http/Controllers/Rest/S3BucketController.php
+++ app/Http/Controllers/Rest/S3BucketController.php
@@ -6,6 +6,7 @@ use Encrypt;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
 
 /**
  * @OA\Tag(
@@ -118,6 +119,56 @@ class S3BucketController extends Controller
 
             return response()->json(['message' => 'File deleted successfully.']);
         } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/s3/file/exists",
+     *     summary="Wasabi If file exists",
+     *     tags={"S3"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *          @OA\JsonContent(
+     *             @OA\Property(property="type", type="string"),
+     *             @OA\Property(property="filename", type="string")
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="X-Auth-Token",
+     *         in="header",
+     *         required=true,
+     *         description="Authorization token",
+     *         example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ2aXN1YWx2ZW5kLWp3dCIsInN1YiI6eyJjbGllbnRfaWQiOjE2MSwiYWRtaW5faWQiOjE1OX0sImlhdCI6MTcxODk2ODA3OSwiZXhwIjoxNzI0MTUyMDc5fQ.LuLaN2o66G1CYxBRa0uheC-ETKD2IiOv3sxEq8QPg7g",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success with api information."
+     *     )
+     * )
+     */
+
+    public function fileExists(Request $request)
+    {
+        $rules = ['filename'      => 'required', 'type'      => 'required|in:image,video,file',];
+        $this->validate($request, $rules);
+
+        $client_id  = md5($request->auth->client_id);
+        $key        = "$client_id/$request->type/$filename";
+
+        try {
+            $result = $this->s3Client->headObject([
+                'Bucket' => env('S3_BUCKET'),
+                'Key' => $key,
+            ]);
+
+            return response()->json(['exists' => true]);
+        } catch (S3Exception $e) {
+            if ($e->getStatusCode() == 404) {
+                return response()->json(['exists' => false]);
+            }
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
