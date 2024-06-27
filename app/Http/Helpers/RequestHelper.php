218ca6415c6b4afaa3e4dfc85682ba9702180869
capanicus-systems <capanicus@gmail.com>
2024-06-27 12:58:25 +0530

    changes

--- app/Http/Helpers/RequestHelper.php
+++ app/Http/Helpers/RequestHelper.php
@@ -9,6 +9,64 @@ use Illuminate\Support\Facades\File;
 
 class RequestHelper
 {
+
+    /**
+     * @OA\Post(
+     *     path="/v1/product/create",
+     *     summary="Products Create",
+     *     tags={"V1"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *          @OA\JsonContent(
+     *             required={"product_name","product_id","product_price","discount_price","product_description","product_image","product_more_info_image"},
+     *             @OA\Property(property="product_name", type="string", default="product_name"),
+     *             @OA\Property(property="product_id", type="string", default="product_id"),
+     *             @OA\Property(property="product_price", type="float", default="10.00"),
+     *             @OA\Property(property="discount_price", type="float", default="08.00"),
+     *             @OA\Property(property="product_description", type="string", default="product_description"),
+     *             @OA\Property(property="product_image", type="string", default="product_image"),
+     *             @OA\Property(property="product_more_info_image", type="string", default="product_more_info_image"),
+     *             @OA\Property(property="client_id", type="integer", default="196"),
+     *             @OA\Property(property="product_sku", type="string", default="product_sku"),
+     *             @OA\Property(property="product_batch_expiray_date", type="string", default="2024-01-01"),
+     *             @OA\Property(property="product_batch_no", type="string", default="product_batch_no"),
+     *             @OA\Property(property="product_caption", type="string", default="product_caption"),
+     *             @OA\Property(property="product_status", type="string", default="1"),
+     *             @OA\Property(property="product_discount_code", type="string", default="product_discount_code"),
+     *             @OA\Property(property="promo_text", type="string", default="promo_text"),
+     *             @OA\Property(property="product_size_amount", type="integer", default="10"),
+     *             @OA\Property(property="product_size_unit", type="string", default="ml"),
+     *             @OA\Property(property="product_classification_no", type="string", default="product_classification_no"),
+     *             @OA\Property(property="product_grading_no", type="string", default="product_grading_no"),
+     *             @OA\Property(property="vend_quantity", type="integer", default="10"),
+     *             @OA\Property(property="more_info_text", type="string", default="more_info_text"),
+     *             @OA\Property(property="product_category", type="string", default="product_category"),
+     *             @OA\Property(property="product_age_verify_required", type="integer", enum= {0, 1}, default="1"),
+     *             @OA\Property(property="product_age_verify_minimum", type="integer", default="15"),
+     *             @OA\Property(property="more_product_images",
+     *                  type="array", 
+     *                  @OA\Items(
+     *                     type="string",
+     *                     default="default-image.jpg"
+     *                 )
+     *              ),
+     *             @OA\Property(property="others", type="object"),
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="X-Auth-Token",
+     *         in="header",
+     *         required=true,
+     *         description="Authorization token",
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success with api information."
+     *     )
+     * )
+     */
+
     public function productRequest($request)
     {
         $other_images = $categories = [];
@@ -32,20 +90,10 @@ class RequestHelper
             $array["others"]            = json_encode($request->others);
         }
 
-        if ($request->has("product_more_image_1") && !empty($request->product_more_image_1) && $request->product_more_image_1 != "null") {
-            $other_images[] = ["uuid" => $array['uuid'], "image" => $request->product_more_image_1];
-        }
-
-        if ($request->has("product_more_image_2") && !empty($request->product_more_image_2) && $request->product_more_image_2 != "null") {
-            $other_images[] = ["uuid" => $array['uuid'], "image" => $request->product_more_image_2];
-        }
-
-        if ($request->has("product_more_image_3") && !empty($request->product_more_image_3) && $request->product_more_image_3 != "null") {
-            $other_images[] = ["uuid" => $array['uuid'], "image" => $request->product_more_image_3];
-        }
-
-        if ($request->has("product_more_image_4") && !empty($request->product_more_image_4) && $request->product_more_image_4 != "null") {
-            $other_images[] = ["uuid" => $array['uuid'], "image" => $request->product_more_image_4];
+        if ($request->has("more_product_images") && count($request->more_product_images) > 0) {
+            foreach ($request->more_product_images as $value) {
+                $other_images[] = ["uuid" => $array['uuid'], "image" => $value];
+            }
         }
 
         if ($request->has("product_category") && !empty($request->product_category) && $request->product_category != "null") {
