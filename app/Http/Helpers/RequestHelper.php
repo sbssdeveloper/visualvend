6dd2d52e8e619f635e50cec72b148c159e803365
capanicus-systems <capanicus@gmail.com>
2024-06-27 16:03:26 +0530

    changes

--- app/Http/Helpers/RequestHelper.php
+++ app/Http/Helpers/RequestHelper.php
@@ -169,7 +169,7 @@ class RequestHelper
     {
 
         $categories = $other_images= [];
-        $data                           = $request->only("verification_method", "product_age_verify_minimum", "product_age_verify_required", "product_size_unit", "product_size_amount", "promo_text", "more_info_text", "product_discount_code", "product_status", "vend_quantity", "product_caption", "product_classification_no", "product_sku", "product_grading_no", "product_batch_expiray_date", "product_batch_no", "product_description", "discount_price", "product_price", "product_id", "product_name");
+        $data                           = $request->only("verification_method", "product_age_verify_minimum", "product_age_verify_required", "product_size_unit", "product_size_amount", "promo_text", "more_info_text", "product_discount_code", "product_status", "vend_quantity", "product_caption", "product_classification_no", "product_sku", "product_grading_no", "product_batch_expiray_date", "product_batch_no", "product_description", "discount_price", "product_price", "product_name", "uuid");
 
         $client_id                      = $request->auth->client_id;
 
