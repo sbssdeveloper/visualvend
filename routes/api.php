985a46f47d87bdf87ebbe0ab11147e4f42c0a6bb
capanicus-systems <capanicus@gmail.com>
2024-06-28 20:11:24 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ $router->group(['prefix' => 'v1'], function () use ($router) {
         $router->post('machine/update',         'MachineController@update');
         $router->post('machine/edit',           'MachineController@edit');
         $router->post('machine/delete',         'MachineController@delete');
-        $router->post('machine/clone',          'MachineController@clone');
+        $router->post('machine/clone',          'MachineController@cloning');
         $router->post('machine/configure',      'MachineController@configure');
         $router->post('machine/products/reset', 'MachineController@reset');
 
