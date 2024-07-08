e759269708a884c67481c02de2847c2cfff91e10
capanicus-systems <capanicus@gmail.com>
2024-07-08 18:30:05 +0530

    updates

--- app/Console/Commands/MakeHelperCommand.php
+++ app/Console/Commands/MakeHelperCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
-class makeHelperCommand extends Command
+class MakeHelperCommand extends Command
 {
     /**
      * The name and signature of the console command.
