bbbc59da21d998e66da3cd68f3b64bd120d73418
capanicus-systems <capanicus@gmail.com>
2024-07-11 17:14:49 +0530

    updates

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -64,19 +64,17 @@ class PaymentsController extends BaseController
         $type       = $request->type;
         $device     = $request->device;
 
+        $badges        = Transaction::selectRaw("SUM(IF(remote_vend_log.status='2',1,0)) as successfull_vends, SUM(IF(remote_vend_log.status NOT IN ('0','1','2','11'),1,0)) as failed_vends, FORMAT(SUM(transactions.amount),2) as total_payments, FORMAT(SUM(IF(transactions.payment_status='SUCCESS',transactions.amount,0)),2) as successfull_payments, FORMAT(SUM(IF(transactions.payment_status='FAILED',transactions.amount,0)),2) as failed_payments");
+        $badges->leftJoin("remote_vend_log", "remote_vend_log.vend_id", "=", "transactions.vend_uuid");
+
         $other = "SUM(IF(pay_method='pay_to_card',1,0)) as total_vends, SUM(IF(remote_vend_log.status IN('3','4','5','6','7','8','00') AND (transactions.payment_status IS NULL OR transactions.payment_status<>'FAILED') AND pay_method='pay_to_card', 1, 0)) as failed_vends, SUM(IF(remote_vend_log.status IN('0','1','11'), 1, 0) AND pay_method='pay_to_card') as in_progress, SUM(IF(remote_vend_log.status='2' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card', 1, 0)) as successfull_vends, SUM(IF(transactions.payment_status='FAILED' AND pay_method='pay_to_card', 1, 0)) as pay_failed, SUM(IF(transactions.id>0 AND pay_method IN ('google_pay','paypal','apple_pay','after_pay'),1,0)) as total_mobile_vends, SUM(IF(transactions.id>0 AND remote_vend_log.status IN('3','4','5','6','7','8','00') AND pay_method IN ('google_pay','paypal','apple_pay','after_pay') AND transactions.payment_status='SUCCESS',1,0)) as failed_mobile_vends, SUM(IF(transactions.id>0 AND payment_status='FAILED' AND pay_method IN ('google_pay','paypal','apple_pay','after_pay'),1,0)) as failed_mobile_payments, SUM(IF(remote_vend_log.status='2' AND pay_method IN ('google_pay','paypal','apple_pay','after_pay'),1,0)) as successfull_mobile_vends";
 
 
-        $model = Transaction::selectRaw("$other, SUM(IF(transactions.payment_status='SUCCESS', amount, 0)) as total_amount, SUM(IF(response LIKE '%DEBIT%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as debit_card_amount,SUM(IF(response LIKE '%CREDIT%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as credit_card_amount, SUM(IF(response LIKE '%VISA%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as visa_amount, SUM(IF(response LIKE '%MASTERCARD%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as mastercard_amount, SUM(IF(response LIKE '%AMEX%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as amex_amount, SUM(IF(pay_method='apple_pay' AND transactions.payment_status='SUCCESS',amount,0)) as apple_amount,  SUM(IF(pay_method='google_pay' AND transactions.payment_status='SUCCESS',amount,0)) as google_amount,  SUM(IF(pay_method='paypal' AND transactions.payment_status='SUCCESS',amount,0)) as paypal_amount,  SUM(IF(pay_method='after_pay' AND transactions.payment_status='SUCCESS',amount,0)) as after_pay_amount");
-        $model->leftJoin('remote_vend_log', 'transactions.transaction_id', '=', 'remote_vend_log.transaction_id');
-
-        $badges        = Transaction::selectRaw("SUM(IF(remote_vend_log.status='2',1,0)) as successfull_vends, SUM(IF(remote_vend_log.status NOT IN ('0','1','2','11'),1,0)) as failed_vends, FORMAT(SUM(transactions.amount),2) as total_payments, FORMAT(SUM(IF(transactions.payment_status='SUCCESS',transactions.amount,0)),2) as successfull_payments, FORMAT(SUM(IF(transactions.payment_status='FAILED',transactions.amount,0)),2) as failed_payments");
-
-        $badges->leftJoin("remote_vend_log", "remote_vend_log.vend_id", "=", "transactions.vend_uuid");
+        $model = Transaction::selectRaw("$other, SUM(IF(transactions.payment_status='SUCCESS', amount, 0)) as total_amount, SUM(IF(response LIKE '%DEBIT%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as debit_card_amount,SUM(IF(response LIKE '%CREDIT%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as credit_card_amount, SUM(IF(response LIKE '%VISA%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as visa_amount, SUM(IF(response LIKE '%MASTERCARD%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as mastercard_amount, SUM(IF(response LIKE '%AMEX%' AND transactions.payment_status='SUCCESS' AND pay_method='pay_to_card',amount,0)) as amex_amount, SUM(IF(pay_method='apple_pay' AND transactions.payment_status='SUCCESS',amount,0)) as apple_amount,  SUM(IF(pay_method='google_pay' AND transactions.payment_status='SUCCESS',amount,0)) as google_amount,  SUM(IF(pay_method='paypal' AND transactions.payment_status='SUCCESS',amount,0)) as paypal_amount,  SUM(IF(pay_method='after_pay' AND transactions.payment_status='SUCCESS',amount,0)) as after_pay_amount")->leftJoin('remote_vend_log', 'transactions.transaction_id', '=', 'remote_vend_log.transaction_id');
 
         if (!empty($start_date) && !empty($end_date)) {
-            $model->whereRaw("transactions.created_at >= '$start_date'")->whereRaw("transactions.created_at <= '$end_date'")->whereRaw("remote_vend_log.id > 0");
-            $badges->whereRaw("transactions.created_at >= '$start_date'")->whereRaw("transactions.created_at <= '$end_date'")->whereRaw("remote_vend_log.id > 0");
+            $model->whereRaw("transactions.created_at >= '$start_date'")->whereRaw("transactions.created_at <= '$end_date'");
+            $badges->whereRaw("transactions.created_at >= '$start_date'")->whereRaw("transactions.created_at <= '$end_date'");
         }
 
         if ($machine_id > 0) {
@@ -86,9 +84,7 @@ class PaymentsController extends BaseController
 
         $model  = $model->first();
 
-        $model->badges  = $badges->first();
-
-
+        $model->badges = $badges->first();
         $model->all_card_payments   = number_format($model->visa_amount + $model->mastercard_amount + $model->amex_amount, 2);
         $model->all_mobile_payments = number_format($model->apple_amount + $model->google_amount + $model->paypal_amount + $model->after_pay_amount, 2);
 
