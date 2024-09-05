2aa985938c622e74c674549d67b9d5d110456108
malkhan <malkhan.malik@capanicus.com>
2024-09-05 20:14:05 +0530

    changes

--- app/Http/Repositories/MachineRepository.php
+++ app/Http/Repositories/MachineRepository.php
@@ -249,7 +249,7 @@ class MachineRepository
         $id = $request->id;
         $model = MachineProductMap::where("id", $id)->first();
         if($model){
-            $extra= Machine::select("machine_name")->where("id",$model->machine_id)->first();
+            $extra= Machine::select("machine_name","machine_client_id")->where("id",$model->machine_id)->first();
             $model->machine_name = $extra->machine_name;
             $model->client_id = $extra->machine_client_id;
         }
