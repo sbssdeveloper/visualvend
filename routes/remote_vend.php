3fb5d5bd493dcf589abaaf41385563ea2dda2f33
malkhan <malkhan.malik@capanicus.com>
2024-09-19 12:00:30 +0530

    changes

--- routes/remote_vend.php
+++ routes/remote_vend.php
@@ -3,5 +3,8 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->group(['prefix' => 'remote/vend'], function () use ($router) {
+    $router->get('/', function () use ($router) {
+        return $router->app->version();
+    });
     $router->post('signup', 'CustomerController@signup');
 });
