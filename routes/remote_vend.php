69d08bc3613abf537d7e135f25d98015daeb85a4
malkhan <malkhan.malik@capanicus.com>
2024-09-19 16:09:38 +0530

    changes

--- routes/remote_vend.php
+++ routes/remote_vend.php
@@ -6,7 +6,8 @@ $router->group(['prefix' => 'remote/vend'], function () use ($router) {
     $router->get('/', function () use ($router) {
         return "Welcome to Remote Vend API Portal";
     });
-    $router->post('signup',                         'RV_CustomerController@signup');
-    $router->post('login',                          'RV_CustomerController@login');
-    $router->get('categories[/{uuid}/{type}]',      'RV_CategoryController@list');
+    $router->post('signup',                             'RV_CustomerController@signup');
+    $router->post('login',                              'RV_CustomerController@login');
+    $router->get('categories[/{machine_id}/{type}]',    'RV_CategoryController@list');
+    $router->get('products[/{machine_id}]',             'RV_ProductController@list');
 });
