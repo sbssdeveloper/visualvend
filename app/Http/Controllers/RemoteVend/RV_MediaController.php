c7e943f019c0bb7ee108a5bc1e9d74103a23a51d
malkhan <malkhan.malik@capanicus.com>
2024-09-26 15:48:16 +0530

    changes

--- app/Http/Controllers/RemoteVend/RV_MediaController.php
+++ app/Http/Controllers/RemoteVend/RV_MediaController.php
@@ -9,10 +9,30 @@ use Illuminate\Http\Request;
 
 class RV_MediaController extends BaseController
 {
+
+    /**
+     * @OA\Get(
+     *     path="/remote/vend/media/info",
+     *     summary="Machine Products List",
+     *     tags={"Remote Vend"},
+     * *    @OA\Parameter(
+     *         name="machine_id",
+     *         in="query",
+     *         required=true,
+     *         @OA\Schema(type="number"),
+     *         description="Machine ID"
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success with api information."
+     *     )
+     * )
+     */
+
     public function homeMedia(Request $request)
     {
         $this->validate($request, ["machine_id" => 'required|exists:machine,id']);
-        
+
         $model = AdvertisementAssign::select("advertisement.ads_path", "advertisement_assign.position", "advertisement_assign.start_date", "advertisement_assign.end_date");
         $model->leftJoin('advertisement', 'advertisement.id', '=', 'advertisement_assign.advertisement_id');
         $model->where(["machine_id" => $request->machine_id, 'is_suspend' => 0]);
